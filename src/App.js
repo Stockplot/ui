d222e7ff9515e4ecf5709a305954a521ef832bc4
Ananya Mahato <ananyaiit123@gmail.com>
2021-05-14 11:21:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+import { Route, Switch } from "react-router";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+
+import Blog from "./components/Blog";
+import {BrowserRouter} from "react-router-dom"
+import PrimarySearchAppBar from "./components/Navbar";
+import Header from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    
+      
+      <Header/>
+     
+      <Switch >
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path='/blog' component={Blog}/>
+      </Switch>
+     
+    </BrowserRouter>
   );
 }
 
