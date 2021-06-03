6f80bfad10a52cd35bcd47c9a4f52153cfadf807
Ananya Mahato <ananyaiit123@gmail.com>
2021-06-03 19:32:44 +0530

    dashboard changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import MenuIcon from '@material-ui/icons/Menu';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import { useLocation } from 'react-router-dom';
-
+import React from "react";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import MoreIcon from "@material-ui/icons/MoreVert";
+import { Link, useLocation } from "react-router-dom";
+import "./styles.css";
 const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
@@ -21,91 +21,93 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    
   },
   title: {
-    display: 'none',
-    [theme.breakpoints.down('sm')]: {
-      display: 'block',
+    display: "none",
+    [theme.breakpoints.down("sm")]: {
+      display: "block",
+      
     },
+    color:"black"
   },
   search: {
-    position: 'relative',
+    position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
+    "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
     marginRight: theme.spacing(2),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(3),
-      width: 'auto',
+      width: "auto",
     },
   },
   searchIcon: {
     padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputRoot: {
-    color: 'inherit',
+    color: "inherit",
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
   sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
     },
   },
   sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
     },
   },
   navDisplayFlex: {
     display: `flex`,
-    justifyContent: `space-between`
+    justifyContent: `space-between`,
   },
   linkText: {
     textDecoration: `none`,
-    textTransform: `uppercase`,
-    color: `white`
+    textTransform: `none`,
+    color: `black`,
   },
   linkTextMobile: {
     textDecoration: `none`,
     textTransform: `uppercase`,
-    color: 'black'
-  }
+    color: "black",
+  },
 }));
 
-export default function PrimarySearchAppBar(props) {
-
+export default function Navbar(props) {
   const location = useLocation();
   let hideMenuIcon = location.pathname !== "/dashboard";
 
   let { mobileDrawerOpen, setMobileDrawerOpen } = props;
 
   const navLinks = [
-    { title: `about us`, path: `/aboutus` },
-    { title: `dashboard`, path: `/dashboard` },
-    { title: `blog`, path: `/blog` },
-    { title: `contact`, path: `/contact` },
-  ]
+    { title: `About us`, path: `/aboutus` },
+    { title: `Dashboard`, path: `/dashboard` },
+    { title: `Blog`, path: `/blog` },
+    { title: `Contact`, path: `/contact` },
+  ];
 
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -131,14 +133,14 @@ export default function PrimarySearchAppBar(props) {
     setMobileDrawerOpen(!mobileDrawerOpen);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
@@ -147,72 +149,86 @@ export default function PrimarySearchAppBar(props) {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-      <a href={'/aboutus'} key={'about us'} className={classes.linkTextMobile}>
-        <p>About Us</p>
-      </a>
+        <Link to="/about us" className={classes.linkTextMobile}>
+          <p>About Us</p>
+        </Link>
       </MenuItem>
       <MenuItem>
-      <a href={'/dashboard'} key={'dashboard'} className={classes.linkTextMobile}>
-        <p>Dashboard</p>
-      </a>
+        <Link
+          to="/dashboard"
+          key={"dashboard"}
+          className={classes.linkTextMobile}
+        >
+          <p>Dashboard</p>
+        </Link>
       </MenuItem>
       <MenuItem>
-      <a href={'/blog'} key={'blog'} className={classes.linkTextMobile}>
-        <p>Blog</p>
-      </a>  
+        <Link to="/blog" key={"blog"} className={classes.linkTextMobile}>
+          <p>Blog</p>
+        </Link>
       </MenuItem>
       <MenuItem>
-      <a href={'/contact'} key={'contact'} className={classes.linkTextMobile}>
-        <p>Contact</p>
-      </a>
+        <Link to="/contact" key={"contact"} className={classes.linkTextMobile}>
+          <p>Contact</p>
+        </Link>
       </MenuItem>
     </Menu>
   );
 
   return (
     <div className={classes.grow}>
-      <AppBar position="static">
+      <AppBar position="static" id="nav" className="nav-bar"  >
         <Toolbar>
-        <IconButton
+          <IconButton
             color="inherit"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
             className={classes.title}
-            style = {hideMenuIcon ? {display: 'none'}:{}}
+            style={hideMenuIcon ? { display: "none" } : {}}
           >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Train Test Stocks
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                color: "black",
+                fontFamily: "Karla",
+              }}
+            >
+              {" "}
+              Train Test Stocks
+            </Link>
           </Typography>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-          <List
-            component="nav"
-            aria-labelledby="main navigation"
-            className={classes.navDisplayFlex}
-          >
-            {navLinks.map(({ title, path }) => (
-              <a href={path} key={title} className={classes.linkText}>
-                <ListItem button>
-                  <ListItemText primary={title} />
-                </ListItem>
-              </a>
-            ))}
-          </List>
+            <List
+              component="nav"
+              aria-labelledby="main navigation"
+              className={classes.navDisplayFlex}
+            >
+              {navLinks.map(({ title, path }) => (
+                <Link to={path} key={title} className={classes.linkText}>
+                  <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                </Link>
+              ))}
+            </List>
           </div>
           <div className={classes.sectionMobile}>
             <IconButton
