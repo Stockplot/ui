c95dd1e86617b2cb612fc391fc2dccf549a5aecd
Ananya Mahato <ananyaiit123@gmail.com>
2021-06-07 16:41:37 +0530

    blog page and article

---
+++ src/components/Article.js
@@ -0,0 +1,30 @@
+import {render} from 'react-dom'
+import ReactMarkdown from 'react-markdown'
+import file from "./Blogs/Terms.md"
+import "./styles.css"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import React, { useState,useEffect} from 'react'
+
+
+function Article() {
+  const [markdown,setMarkdown]=useState("")
+  useEffect(() => {
+    fetch(file)
+      .then((res) => res.text())
+      .then((text) => setMarkdown(text));
+  }, []);
+  console.log(markdown)
+  return (
+    <div className="blog">
+    
+     <ReactMarkdown>
+       
+       {markdown}
+       
+     </ReactMarkdown>
+     
+    </div>
+  )
+}
+
+export default Article
